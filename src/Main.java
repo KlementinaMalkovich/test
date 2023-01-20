a032413162d22839561f5d3da111b1887e1d4617
Klementina <tanysik7@mail.ru>
2023-01-20 13:57:40 +0300

    Firs commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        String color = "green";
+        result(color);
+    }
+
+
+    public static void result (String color){
+        switch (color) {
+            case "red":
+                System.out.println("Stop");
+                break;
+            case "yellow":
+                System.out.println("Wait");
+                break;
+            case "green":
+                System.out.println("Go");
+                break;
+            default:
+                System.out.println("Wrong color");
+        }
+    }
+}
